040d4fe10404d4700932b4f249d4e4cb91fba522
Zac Smith <zrs3141592@gmail.com>
2020-02-27 16:27:42 -0700

    create login with handleChange and handleSubmit

--- animals/src/components/Login.js
+++ animals/src/components/Login.js
@@ -1,12 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { axiosWithAuth } from "../utils/axiosWithAuth.js";
 
 export default function Login(props) {
-    // How can we log in? What do we need to do?
+  // How can we log in? What do we need to do?
+  const [login, setLogin] = useState({
+    username: "",
+    password: ""
+  });
 
-    return (
-        <div>
-            <h1>Welcome to the Safari App!</h1>
-            <h2>I can't show you more until you log in. Please build out a login.</h2>
-        </div>
-    )
+  const handleChange = event => {
+    setLogin({
+      ...login,
+      [event.target.name]: event.target.value
+    });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post()
+      .then(response => {
+        console.log("response", response);
+        localStorage.setItem("token", response.data.payload);
+        // might have to change the route
+        props.history.push("/creatures");
+      })
+      .catch(error => {
+        console.log(`Login error:  ${error}`);
+      });
+  };
+
+  return (
+    <div>
+      <form className="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="username"
+          name="username"
+          value={login.username}
+          label="username"
+          onChange={handleChange}
+          className="input"
+        />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          value={login.password}
+          label="password"
+          onChange={handleChange}
+          className="input"
+        />
+        <button className="start">Login</button>
+      </form>
+    </div>
+  );
 }

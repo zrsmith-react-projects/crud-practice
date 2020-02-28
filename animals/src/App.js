ef9e59457c33fee59a7916070f46650dd08b67a3
Zac Smith <zrs3141592@gmail.com>
2020-02-27 17:52:57 -0700

    create isLoading

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
+import PrivateRoute from "./utils/PrivateRoute.js";
 import Login from "./components/Login.js";
 import Header from "./components/Header.js";
+import AnimalDashboard from "./components/AnimalDashboard";
 
 export default function App() {
-
   return (
     <div className="App">
       <Header />
       <Switch>
         {/* Build out a Private Route */}
+        <PrivateRoute exact path="/creatures" component={AnimalDashboard} />
         <Route exact path="/login" component={Login} />
       </Switch>
     </div>
   );
-};
+}
--- animals/src/components/AnimalDashboard.js
+++ animals/src/components/AnimalDashboard.js
@@ -2,18 +2,36 @@ import React, { useState, useEffect } from "react";
 
 import AnimalForm from "./AnimalForm.js";
 import AnimalList from "./AnimalsList.js";
+import { axiosWithAuth } from "../utils/axiosWithAuth.js";
 
 export default function AnimalDashboard() {
-    
-    const [ animals, setAnimals ] = useState([]);
-    
-    // How can get fetch the data from the server when the component mounts?
-    // How can we capture and set that data to state?
+  const [animals, setAnimals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    return(
-        <div className="dash">
-            <AnimalForm animals={animals} updateAnimals={setAnimals} />
-            <AnimalList animals={animals} />
-        </div>
-    )
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      axiosWithAuth()
+        .get("animals")
+        .then(response => {
+          console.log(response.data);
+          setAnimals(response.data);
+          setIsLoading(false);
+        })
+        .catch(error => {
+          console.log(
+            `Error fetching animals, you should not beat them ${error.response}`
+          );
+          setIsLoading(false);
+        });
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="dash">
+      {isLoading ? "LOADING" : ""}
+      <AnimalForm animals={animals} updateAnimals={setAnimals} />
+      <AnimalList animals={animals} />
+    </div>
+  );
 }
